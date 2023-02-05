29dd6e31078e12ef2660689e39fcb7f009fa6a58
App-lied <alanphillips2001@gmail.com>
2023-02-05 15:07:57 +0000

    Pageified book browsing

--- LibeRate/LibeRate/Models/Book.cs
+++ LibeRate/LibeRate/Models/Book.cs
@@ -6,13 +6,15 @@ namespace LibeRate.Models
 {
     public class Book
     {
+        public string Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string ImageURL { get; set; }
         public int DifficultyRating { get; set; }
 
-        public Book(string title, string author, string imageURL, int difficultyRating)
+        public Book(string id, string title, string author, string imageURL, int difficultyRating)
         {
+            Id = id;
             Title = title;
             Author = author;
             ImageURL = imageURL;
--- LibeRate/LibeRate/ViewModels/BooksViewModel.cs
+++ LibeRate/LibeRate/ViewModels/BooksViewModel.cs
@@ -3,6 +3,7 @@ using LibeRate.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using Xamarin.Forms;
 
@@ -10,28 +11,62 @@ namespace LibeRate.ViewModels
 {
     public class BooksViewModel : BaseViewModel
     {
+        IBookService bookService;
         public ObservableCollection<Book> Books { get; set; }
+        private int _pageNumber;
+        private bool _previousButtonVisible;
+        public int ItemsPerPage { get; set; }
         public Command RefreshCommand { get; }
         public Command ViewDetailCommand { get; }
+        public Command NextPageCommand { get; }
+        public Command PreviousPageCommand { get; }
 
         public BooksViewModel()
         {
             Title = "Browse";
 
+            PageNumber= 1;
+            ItemsPerPage = 25;
+            PreviousButtonVisible = false;
+
             RefreshCommand = new Command(Refresh);
             ViewDetailCommand = new Command(ViewDetail);
+            NextPageCommand = new Command(NextPage);
+            PreviousPageCommand = new Command(PreviousPage);
+
+            bookService = DependencyService.Get<IBookService>();
             Books = new ObservableCollection<Book>();
             LoadBooks();
         }
 
+        public int PageNumber
+        {
+            get => _pageNumber;
+            set
+            {
+                _pageNumber = value;
+                OnPropertyChanged(nameof(PageNumber));
+            }
+        }
+
+        public bool PreviousButtonVisible
+        {
+            get => _previousButtonVisible;
+            set
+            {
+                _previousButtonVisible = value;
+                OnPropertyChanged(nameof(PreviousButtonVisible));
+            }
+        }
+
         private async void LoadBooks()
         {
-            IBookService bookService = DependencyService.Get<IBookService>();
-            List<Book> books = await bookService.GetBooks("english");
-            foreach(Book book in books)
+            Books.Clear();
+            List<Book> result = await bookService.GetBooks("english", PageNumber, ItemsPerPage);
+            foreach(Book book in result)
             {
                 Books.Add(book);
-            } 
+            }
         }
 
         private async void Refresh()
@@ -40,6 +75,27 @@ namespace LibeRate.ViewModels
 
         private async void ViewDetail()
         {
+
+        }
+
+        private void NextPage()
+        {
+            Books.Clear();
+            PageNumber++;
+            PreviousButtonVisible = true;
+            LoadBooks();
+
+        }
+
+        private void PreviousPage()
+        {
+            Books.Clear();
+            PageNumber--;
+            if(PageNumber == 1)
+            {
+                PreviousButtonVisible= false;
+            }
+            LoadBooks();
 
         }
     }
