2a580c4fca4be0814a7b26561012f27d87008bcb
App-lied <alanphillips2001@gmail.com>
2023-02-22 16:36:06 +0000

    proper document instantiation

--- LibeRate/LibeRate.Android/Services/LibraryService.cs
+++ LibeRate/LibeRate.Android/Services/LibraryService.cs
@@ -61,7 +61,7 @@ namespace LibeRate.Droid.Services
             List<string> bookIds= new List<string>();
             foreach(var book in books)
             {
-                if(book.Value == status)
+                if(book.Value == status && book.Key != "")
                 {
                     bookIds.Add(book.Key);
                 }
