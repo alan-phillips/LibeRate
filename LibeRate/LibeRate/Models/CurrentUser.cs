edc6c3c6e56966b3525a33dec6b75322a9973b59
App-lied <alanphillips2001@gmail.com>
2023-03-21 17:28:29 +0000

    created CurrentUser singleton

---
+++ LibeRate/LibeRate/Models/CurrentUser.cs
@@ -0,0 +1,33 @@
+﻿using LibeRate.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LibeRate.Models
+{
+    public class CurrentUser : User
+    {
+        private CurrentUser()
+        {
+            IFirebaseAuthentication auth = DependencyService.Get<IFirebaseAuthentication>();
+            if (auth.IsSignedIn())
+            {
+                Task.Run(async () => await LoadUser(auth.GetUserID()));
+            }
+        }
+
+        private static CurrentUser _instance = new CurrentUser();
+        public static CurrentUser Instance
+        {
+            get { return _instance; }
+        }
+
+        private async Task LoadUser(string id)
+        {
+            IUserService userService = DependencyService.Get<IUserService>();
+            _instance = (CurrentUser)await userService.GetUser(id);
+        }
+    }
+}
